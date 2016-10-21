4f466f6d447994f66999c92be3b9db630bd4081a
Jeremy Foster <jeremy.foster@live.com>
2016-10-20 23:29:59 -0700

    add iot hub client and organize code a bit

--- device/index.ts
+++ device/index.ts
@@ -1,18 +1,23 @@
-import * as oxford from 'project-oxford';
 import * as Camera from 'camerapi';
+import * as oxford from 'project-oxford';
 import * as fs from 'fs';
+import * as device from 'azure-iot-device';
+import * as deviceAmqp from 'azure-iot-device-amqp';
 
-let clientFromConnectionString = require('azure-iot-device-amqp').clientFromConnectionString;
-let Message = require('azure-iot-device').Message;
-
-let client = new oxford.Client('d7889254b9244ec1ba54e8cf154ff359');
+let cogClient = new oxford.Client('d7889254b9244ec1ba54e8cf154ff359');
+var connectionString = 'HostName=cfhub.azure-devices.net;DeviceId=<deviceid>;SharedAccessKey=0ZDUbn/1hTIgKtl4sfLYD+peW63aNnQgQhkVCi99i88=';
+var hubClient = deviceAmqp.clientFromConnectionString(connectionString);
 
 let cam = new Camera();
 cam.baseFolder('.');
 cam.takePicture('picture.png',(file,error) => {
-    client.vision.analyzeImage({ path: 'picture.png', Tags: true }).then(result => {
+    cogClient.vision.analyzeImage({ path: 'picture.png', Tags: true }).then(result => {
         let tags = result.tags.filter(t => t.confidence >= .5).map(t => t.name);
         console.log(JSON.stringify(tags, undefined, 4));
         fs.unlinkSync('picture.png'); //delete the picture
+
+        var message = new device.Message(JSON.stringify({ deviceId: '<deviceid>', tags: tags }));
+        hubClient.sendEvent(message);
+
     });
 });
