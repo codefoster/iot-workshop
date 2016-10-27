cda4afdf129805275317108833f59ce19d8b3309
Jeremy Foster <jeremy.foster@live.com>
2016-10-26 22:21:41 -0700

    add gpio, rearrange code

--- device/index.ts
+++ device/index.ts
@@ -1,3 +1,5 @@
+import * as five from 'johnny-five';
+import * as raspi from 'raspi-io';
 import * as Camera from 'camerapi';
 import * as oxford from 'project-oxford';
 import * as fs from 'fs';
@@ -8,26 +10,57 @@ let cogClient = new oxford.Client('d7889254b9244ec1ba54e8cf154ff359');
 let connectionString = 'HostName=iot-workshop-hub.azure-devices.net;DeviceId=device1;SharedAccessKey=zyislRKCFj5k916xvNRyB0JPihQpad/56tzTMZMWpdk=';
 let hubClient = deviceAmqp.clientFromConnectionString(connectionString);
 
-hubClient.open(err => {
-    if (err) console.log(err)
-    else {
-        let cam = new Camera();
-        cam.baseFolder('.');
-        cam.takePicture('picture.png', (file, error) => {
-            if (error) console.log(error);
-            cogClient.vision.analyzeImage({ path: 'picture.png', Tags: true })
-                .then(result => {
-                    fs.unlinkSync('picture.png'); //delete the picture
-                    
-                    let message = new device.Message(JSON.stringify({ deviceId: 'device1', tags: result.tags }));
-                    hubClient.sendEvent(message, (err, res) => {
-                        if (err) console.log(err);
-                        else console.log(`Sent ${JSON.stringify(result.tags)} to your IoT Hub`);
-                        hubClient.close((err, res) => {
-                            if (err) console.log(err);
-                        })
-                    });
+//establishing connection to gpio
+log('establishing connection to gpio...');
+let board = new five.Board({ io: new raspi() });
+board.on('ready', () => {
+    let led = new five.Led('GPIO26');
+    let button = new five.Button('GPIO20');
+    led.stop().off();
+
+    //open connection to iot hub
+    log('connecting to iot hub...');
+    hubClient.open(err => {
+        if (err)
+            log(err.message)
+        else {
+            log('READY');
+            led.stop().off();
+
+            let cam = new Camera();
+            cam.baseFolder('.');
+            button.on('press', () => {
+                led.blink(500);
+                log('taking a picture...');
+                cam.takePicture('picture.png', (file, error) => {
+                    if (error) log(error);
+                    else {
+                        //analyzing image
+                        log('analyzing image...');
+                        cogClient.vision.analyzeImage({ path: 'picture.png', Tags: true })
+                            .then(result => {
+                                fs.unlinkSync('picture.png'); //delete the picture
+
+                                //sending message to iot hub
+                                log('sending message to iot hub...');
+                                let message = new device.Message(JSON.stringify({ deviceId: 'device1', tags: ['foo', 'baz', 'bar'] }));
+                                hubClient.sendEvent(message, (err, res) => {
+                                    if (err) log(err.message);
+                                    else {
+                                        log(`Sent ${JSON.stringify(result.tags)} to your IoT Hub`);
+                                        log('READY');
+                                    }
+                                    led.stop().off();
+                                });
+                            });
+                    }
                 });
-        });
-    }
+            })
+        }
+    })
 })
+
+
+function log(msg: string) {
+    console.log(msg);
+}
