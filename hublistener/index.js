aec0cf76a46a9a16b662339c078d733ddfd1ee88
Jeremy Foster <jeremy.foster@live.com>
2017-11-23 07:18:08 +0200

    change hublistener to javascript

--- hublistener/index.js
+++ hublistener/index.js
@@ -1,18 +1,17 @@
-"use strict";
-const eh = require('azure-event-hubs');
-let connectionString = 'HostName=cfhub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=OrBlnCd0JvzPdr5VzSzbryQ2PunvonBcyWrsLobnVHs=';
-var client = eh.Client.fromConnectionString(connectionString);
-client.open()
-    .then(client.getPartitionIds.bind(client))
-    .then(function (partitionIds) {
-    return partitionIds.map(function (partitionId) {
-        return client.createReceiver('$Default', partitionId, { 'startAfterTime': Date.now() })
-            .then(function (receiver) {
-            console.log('Created partition receiver: ' + partitionId);
-            receiver.on('errorReceived', err => console.log(err.message));
-            receiver.on('message', m => console.log(JSON.stringify(m.body)));
-        });
-    });
-})
+let eh = require('azure-event-hubs');
+
+var hubClient = eh.Client.fromConnectionString(process.env.HUB_CONNECTION_STRING);
+
+hubClient.open()
+    .then(hubClient.getPartitionIds.bind(hubClient))
+    .then(pids => 
+        pids.map(pid => 
+            hubClient.createReceiver('$Default', pid, { 'startAfterTime': Date.now() })
+                .then(receiver => {
+                    console.log('Created partition receiver: ' + pid)
+                    receiver.on('errorReceived', err => console.log(err.message));
+                    receiver.on('message', m => console.log(JSON.stringify(m.body)));
+                })
+        )
+    )
     .catch(err => console.log(err.message));
-//# sourceMappingURL=index.js.map
